2b8272f0155751eb5b3b30382b18bdd45cf78cc7
GeorgeProjects <liguozhengsdu@gmail.com>
2021-02-01 15:59:36 +0800

    save the dim reduction results

--- import_csv.py
+++ import_csv.py
@@ -1,6 +1,6 @@
 import csv
 
-def get_dsv_data(file_name):
+def read_csv_data(file_name):
     data_item_list = []
     with open(file_name, newline='') as f:
         reader = csv.reader(f)
@@ -8,5 +8,13 @@ def get_dsv_data(file_name):
             data_item_list.append(row)
     return data_item_list
 
+def save_csv_data(file_name, data_item_list):
+    with open(file_name, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(data_item_list)
+
 # if __name__ == '__main__':
-#     get_dsv_data()
+#     # read_csv_data()
+#     data_item_list = [[1,2], [3, 4]]
+#     file_name = 'results.csv'
+#     save_csv_data(file_name, data_item_list)
--- tsne.py
+++ tsne.py
@@ -1,13 +1,14 @@
 import numpy as np
 from sklearn.manifold import TSNE
 
-from import_csv import get_dsv_data
+from import_csv import read_csv_data, save_csv_data
 
 if __name__ == '__main__':
-    file_name = 'results.csv'
-    data_item_list = get_dsv_data(file_name)
+    file_name = 'embeddings-results.csv'
+    file_name_2d = 'embeddings-results-2d.csv'
+    data_item_list = read_csv_data(file_name)
     X = np.array(data_item_list)
     X_embedded = TSNE(n_components=2).fit_transform(X)
     reduction_shape = X_embedded.shape
-    print(reduction_shape)
-    print(X_embedded)
+    save_csv_data(file_name_2d, X_embedded)
+    
