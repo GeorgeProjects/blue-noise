c48adb9b2b3b00531520a7b1061e4e80b459c9d3
GeorgeProjects <liguozhengsdu@gmail.com>
2021-02-01 15:33:26 +0800

    compute reduction results

--- tsne.py
+++ tsne.py
@@ -6,9 +6,8 @@ from import_csv import get_dsv_data
 if __name__ == '__main__':
     file_name = 'results.csv'
     data_item_list = get_dsv_data(file_name)
-    print(data_item_list)
-    # X = np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
-    # X_embedded = TSNE(n_components=2).fit_transform(X)
-    # reduction_shape = X_embedded.shape
-    # print(reduction_shape)
+    X = np.array(data_item_list)
+    X_embedded = TSNE(n_components=2).fit_transform(X)
+    reduction_shape = X_embedded.shape
+    print(reduction_shape)
     # print(X_embedded)
