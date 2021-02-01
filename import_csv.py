ede0d0c39129b31edfde74a3a7f40421f9079468
GeorgeProjects <liguozhengsdu@gmail.com>
2021-02-01 15:30:55 +0800

    complement import csv

--- import_csv.py
+++ import_csv.py
@@ -1,11 +1,13 @@
 import csv
 
 def get_dsv_data(file_name):
+    data_item_list = []
     with open(file_name, newline='') as f:
         reader = csv.reader(f)
         for row in reader:
             print(row)
-        print(reader)
+            data_item_list.append(row)
+    return data_item_list
 
 # if __name__ == '__main__':
 #     get_dsv_data()
