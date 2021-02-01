7b866120b281e2d88c54ad535ffbc8b2fb2eef16
GeorgeProjects <liguozhengsdu@gmail.com>
2021-02-01 15:00:52 +0800

    add log results

--- tsne.py
+++ tsne.py
@@ -5,3 +5,4 @@ X = np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
 X_embedded = TSNE(n_components=2).fit_transform(X)
 reduction_shape = X_embedded.shape
 print(reduction_shape)
+print(X_embedded)
