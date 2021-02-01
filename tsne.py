4275f99ed960027f1ec74ea69e51057a1b254461
GeorgeProjects <liguozhengsdu@gmail.com>
2021-02-01 15:00:05 +0800

    add log

--- tsne.py
+++ tsne.py
@@ -3,4 +3,5 @@ from sklearn.manifold import TSNE
 
 X = np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
 X_embedded = TSNE(n_components=2).fit_transform(X)
-X_embedded.shape
+reduction_shape = X_embedded.shape
+print(reduction_shape)
